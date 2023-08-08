56c6979762423857e9e40b5102522ffb6c9b2e8c
Pablo Santana <pablosntojd@gmail.com>
2023-08-08 02:11:26 +0100

    feat: logic to check if there's or not a winner

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -19,17 +19,49 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
   const [turn, setTurn] = useState(TURNS.X)
+  //null means no winner, false means draw
+  const [winner, setWinner] = useState(null)
+  
+  const checkWinner = (boardToCheck) => {
+    // check all the combos to see who wins
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && // 0 -> to be × or o
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a] // winner × or o
+      }
+    }
+    // if there's no winner
+    return null
+  }
 
   const updateBoard = (index) => {
+    //don't update if already written
+    if (board[index]) return
+    //update board
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-
+    // change turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
   }
