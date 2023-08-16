6466441758c1ccd8d2e8ce9b54a19d11e81acdd4
Pablo Santana <pablosntojd@gmail.com>
2023-08-16 01:55:52 +0100

    fix: separate the useeffect in two

--- 02-mouse-follower/src/App.jsx
+++ 02-mouse-follower/src/App.jsx
@@ -6,24 +6,30 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App() {
-  const [fact, setFact] = useState('lorem ipsum cat fact whatver')
-  const [imageUrl, setImageUrl] = useState('lorem ipsum cat fact whatver')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
+  // get the fact quotes
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
-        const {fact} = data
-        setFact(data.fact)
-        const threeFirstWords = fact.split(' ', 3).join(' ')
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-        .then(res => res.json())
-        .then(response => {
-          const { url } = response
-          setImageUrl(url)
-        })
+        const { fact } = data
+        setFact(fact)
       })
   }, [])
+
+  // get the image from the fact
+  useEffect(() => {
+    if(!fact) return
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+    .then(res => res.json())
+    .then(response => {
+      const { url } = response
+      setImageUrl(url)
+    })
+  }, [fact])
 
   return (
     <main>
