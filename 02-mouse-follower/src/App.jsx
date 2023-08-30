1c5945cf4ad3eb4496d8219fbe79ce4f32be7b07
Pablo Santana <pablosntojd@gmail.com>
2023-08-30 10:56:27 +0100

    ref: exported getRandomFact logic

--- 02-mouse-follower/src/App.jsx
+++ 02-mouse-follower/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { getRandomFact } from './services/facts'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
@@ -9,20 +9,14 @@ export function App() {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
-  // get the fact quotes 
-  const getRandomFact = () => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
-  }
-  useEffect(() => {getRandomFact, []})
+  useEffect(() => {
+    getRandomFact().then(setFact)}
+    , [])
 
   // get the image from the fact
   useEffect(() => {
     if(!fact) return
+
     const threeFirstWords = fact.split(' ', 3).join(' ')
     fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
     .then(res => res.json())
@@ -30,9 +24,13 @@ export function App() {
       const { url } = response
       setImageUrl(url)
     })
+
   }, [fact])
 
-  const handleClick = () => {getRandomFact()}
+  const handleClick = async() => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
 
   return (
     <main>
